b28dcc314045a1c52cbef09e0ba3f4d500cd4c9b
minpeter <kali2005611@gmail.com>
2020-06-25 22:14:42 +0900

    toDoList ver.2

--- todo.js
+++ todo.js
@@ -4,16 +4,31 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
     const TODOS_LS = "toDos";
 
+    const toDos = []
+
+    function saveToDos() {
+        localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+    }
+
     function paintToDo(text) {
-        const li = document.createElement("li"),
-        delBtn = document.createElement("button");
-        delBtn.innerText = "X";
+        const li = document.createElement("li");
+        const delBtn = document.createElement("button");
         const span = document.createElement("span");
+        const newId = toDos.length + 1;
+        delBtn.innerText = "X";
         span.innerText = text;
         li.appendChild(delBtn);
         li.appendChild(span);
+        li.id = newId;
         toDoList.appendChild(li);
-      }
+        const toDoObj = {
+            text: text,
+            id: newId
+        };
+        saveToDos();
+        toDos.push(toDoObj);
+    }
+    
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -23,8 +38,12 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     }
 
     function loadToDos() {
-        const toDos = localStorage.getItem(TODOS_LS);
-        if (toDos !== null) {
+        const loadedToDos = localStorage.getItem(TODOS_LS);
+        if (loadToDos !== null) {
+            const parsedToDos = JSON.parse(loadedToDos);
+            parsedToDos.forEach(function(toDo){
+                paintToDo(toDo.text);
+            })
         }
       }
       
