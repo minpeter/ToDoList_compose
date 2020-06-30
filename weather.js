825b6e4dbdf20d74b772ed6de27ad9af53caaebf
minpeter <kali2005611@gmail.com>
2020-06-30 22:53:57 +0900

    weather_Api

--- weather.js
+++ weather.js
@@ -1,9 +1,23 @@
+const API_KEY = "f274a5616d927108ae79c369991175e5"; //OpenWeatherMap API_Key
 const COORDS = 'coords';
 
+function getWeather(lat,lng) {
+    fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`);
+}
 
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+};
 
 function handleGeoSucces(position) {
-    
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 function handleGeoError() {
@@ -15,10 +29,12 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadCoords = localStorage.getItem(COORDS);
-    if(loadCoords === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
         askForCoords();
     } else {
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude,parseCoords.longitude);
 
     }
 }
