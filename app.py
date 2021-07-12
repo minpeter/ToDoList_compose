f8de861d7001779bc1cd2a614e5c916c3291a7b8
minpeter <kali2005611@gmail.com>
2021-07-12 06:29:01 +0000

    post -> get

--- app.py
+++ app.py
@@ -3,47 +3,59 @@ import dbCtrl
 
 app = Flask (__name__)
 
-@app.route("/addUser", methods = ['POST'])
+@app.route("/addUser", methods = ['GET'])
 def AddUser():
-    userData = request.get_json()
-    returnV = dbCtrl.addUser(userData["userName"], userData["passwordH"], userData["email"])
+    userName = request.args.get("userName")
+    passwordH = request.args.get("passwordH")
+    email = request.args.get("email")
+    returnV = dbCtrl.addUser(userName, passwordH, email,)
     return jsonify(returnV)
 
 
-@app.route("/login", methods = ['POST'])
+@app.route("/login", methods = ['GET'])
 def Login():
-    loginData = request.get_json()
-    returnV = dbCtrl.login(loginData["userName"], loginData["passwordH"])
+    userName = request.args.get("userName")
+    passwordH = request.args.get("passwordH")
+    returnV = dbCtrl.login(userName, passwordH)
     return jsonify(returnV)
 
-@app.route("/addTodo", methods = ['POST'])
+@app.route("/addTodo", methods = ['GET'])
 def AddTodo():
-    todoData = request.get_json()
-    returnV = dbCtrl.addTodo(todoData["userId"], todoData["todo"], todoData["endday"], todoData["importance"])
+    userId = request.args.get("userId")
+    todo = request.args.get("todo")
+    endday = request.args.get("endday")
+    importance = request.args.get("importance")
+    returnV = dbCtrl.addTodo(userId, todo, endday, importance)
     return jsonify(returnV)
 
-@app.route("/readTodo", methods = ['POST'])
+@app.route("/readTodo", methods = ['GET'])
 def ReadTodo():
-    todoData = request.get_json()
-    returnV = dbCtrl.readTodo(todoData["userId"])
+    userId = request.args.get("userId")
+    returnV = dbCtrl.readTodo(userId)
     return jsonify(returnV)
 
-@app.route("/delTodo", methods = ['POST'])
+@app.route("/delTodo", methods = ['GET'])
 def DelTodo():
-    todoData = request.get_json()
-    returnV = dbCtrl.delTodo(todoData["id"], todoData["userId"])    
+    id = request.args.get("id")
+    userId = request.args.get("userId")
+    returnV = dbCtrl.delTodo(id, userId)
     return jsonify(returnV)
 
-@app.route("/editTodo", methods = ['POST'])
+@app.route("/editTodo", methods = ['GET'])
 def EditTodo():
-    todoData = request.get_json()
-    returnV = dbCtrl.editTodo(todoData["id"], todoData["userId"], todoData["editSel"], todoData["text"])    
+    id = request.args.get("id")
+    userId = request.args.get("userId")
+    editSel = request.args.get("editSel")
+    text = request.args.get("text")
+    returnV = dbCtrl.editTodo(id, userId, editSel, text)    
     return jsonify(returnV)
 
-@app.route("/todoComplete", methods = ['POST'])
+@app.route("/todoComplete", methods = ['GET'])
 def TodoComplete():
-    todoData = request.get_json()
-    returnV = dbCtrl.todoComplete(todoData["id"], todoData["userId"], todoData["tf"])
+    id = request.args.get("id")
+    userId = request.args.get("userId")
+    tf = request.args.get("tf")
+    returnV = dbCtrl.todoComplete(id, userId, tf)
     return jsonify(returnV)
 
 if __name__ == '__main__':
